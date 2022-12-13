f99b11471432ab395aee64a9393d44daf04ad219
AhmedKhchai <a.khchai@gmail.com>
2022-12-13 10:07:47 +0100

    bbjacks is the best

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 /* CELEBP27 */
 #define _OPEN_THREADS
+#define _GNU_SOURCE
+
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+
 void *handler(void *arg) {
 
   printf("we're going to sleep for '%d'\n", arg);
   sleep('%d', arg);
-  for (int i = 0; i < 1000000; i++) {
+  for (int i = 0; i < 100000000; i++) {
   }
 
 }
-
-main() {
+void *limiteThreadCpu(pthread_t* thread){
+    cpu_set_t cpuset;
+    thread = pthread_self();
+    CPU_ZERO(&cpuset);
+	CPU_SET(1, &cpuset);
+	pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+}
+int main() {
   pthread_t tOne;
   pthread_t tTwo;
   pthread_t tThree;
   pthread_t tFour;
+  limiteThreadCpu(tOne);
+  limiteThreadCpu(tTwo);
+  limiteThreadCpu(tThree);
+  limiteThreadCpu(tFour);
 
   pthread_attr_t attr;
   pthread_attr_init (&attr);
@@ -48,7 +61,9 @@ main() {
     perror("pthread_create() error");
     exit(1);
   };
-
-
-
+  int pthread_join(pthread_t tOne, void **retval);
+  int pthread_join(pthread_t tTwo, void **retval);
+  int pthread_join(pthread_t tThree, void **retval);
+  int pthread_join(pthread_t tFour, void **retval);
+  return 0;
 }
