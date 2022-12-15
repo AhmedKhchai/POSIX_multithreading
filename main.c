844a97b557ececd39224d7117cff291b240a6060
AhmedKhchai <a.khchai@gmail.com>
2022-12-15 10:56:31 +0100

    soso

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
-/* CELEBP27 */
 #define _OPEN_THREADS
 #define _GNU_SOURCE
-
+#include <sys/time.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <sys/time.h>
 
-void *handler(void *arg) {
-
+void handler(int arg) {
+  struct timeval t0, t1, dt;
+  gettimeofday(&t0, NULL);
   printf("we're going to sleep for '%d'\n", arg);
-  sleep('%d', arg);
-  for (int i = 0; i < 100000000; i++) {
+  sleep('%d',arg);
+  for (int i = 0; i < 1000000; i++) {
   }
-
+  gettimeofday(&t1, NULL);
+  timersub(&t1, &t0, &dt);
+  fprintf(stderr, "the thread took %d.%06d sec\n",
+   dt.tv_sec, dt.tv_usec);
 }
-pthread_t *limiteThreadCpu(pthread_t* thread){
+
+pthread_t limiteThreadCpu(pthread_t* thread){
     cpu_set_t cpuset;
     thread = pthread_self();
     CPU_ZERO(&cpuset);
@@ -23,6 +28,7 @@ pthread_t *limiteThreadCpu(pthread_t* thread){
 	pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
 	return thread;
 }
+
 int main() {
   pthread_t tOne;
   pthread_t tTwo;
@@ -32,7 +38,7 @@ int main() {
   tTwo = limiteThreadCpu(tTwo);
   tThree = limiteThreadCpu(tThree);
   tFour = limiteThreadCpu(tFour);
-
+  // this function takes a thread and is supposed to specify the cpu core that should be assigned to the passed thread
   pthread_attr_t attr;
   pthread_attr_init (&attr);
   pthread_attr_setinheritsched(&attr,PTHREAD_EXPLICIT_SCHED);
@@ -40,32 +46,36 @@ int main() {
 
   struct sched_param param;
   pthread_attr_getschedparam (&attr, &param);
-  (param.sched_priority)++;
+  param.sched_priority = 10;
   pthread_attr_setschedparam (&attr, &param);
 
   if (pthread_create(&tOne, &attr, handler, 1) != 0) {
     perror("pthread_create() error");
     exit(1);
   }
-  (param.sched_priority)++;
+  param.sched_priority = 20;
+  pthread_attr_setschedparam (&attr, &param);
     if (pthread_create(&tTwo, &attr, handler, 2) != 0) {
     perror("pthread_create() error");
     exit(1);
   }
-  (param.sched_priority)++;
+  param.sched_priority = 30;
+  pthread_attr_setschedparam (&attr, &param);
     if (pthread_create(&tThree, &attr, handler, 3)!= 0) {
     perror("pthread_create() error");
     exit(1);
   }
-  (param.sched_priority)++;
+  param.sched_priority = 40;
+  pthread_attr_setschedparam (&attr, &param);
     if (pthread_create(&tFour, &attr, handler, 4) != 0) {
     perror("pthread_create() error");
     exit(1);
   };
-  int * pInt = NULL;
-  int pthread_join(tOne, pInt);
-  int pthread_join(tTwo, pInt);
-  int pthread_join(tThree, pInt);
-  int pthread_join(tFour, pInt);
+
+  pthread_join(tOne, NULL);
+  pthread_join(tTwo, NULL);
+  pthread_join(tThree, NULL);
+  pthread_join(tFour, NULL);
+
   return 0;
 }
