a0a21ce7fcc274a83f5eb05b5f8e6eeab6181c7a
AhmedKhchai <a.khchai@gmail.com>
2022-12-15 12:10:54 +0100

    poc

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+/* CELEBP27 */
 #define _OPEN_THREADS
 #define _GNU_SOURCE
 #include <sys/time.h>
@@ -9,15 +10,17 @@
 
 void handler(int arg) {
   struct timeval t0, t1, dt;
-  gettimeofday(&t0, NULL);
   printf("we're going to sleep for '%d'\n", arg);
-  sleep('%d',arg);
-  for (int i = 0; i < 1000000; i++) {
+  sleep(arg);
+  gettimeofday(&t0, NULL);
+  printf("wake up '%d'\n", arg);
+  for (int i = 0; i < 100000; i++) {
+    for (int j = 0; j < 10000; j++) {
+    }
   }
   gettimeofday(&t1, NULL);
   timersub(&t1, &t0, &dt);
-  fprintf(stderr, "the thread took %d.%06d sec\n",
-   dt.tv_sec, dt.tv_usec);
+  fprintf(stderr, "the thread %d took %d.%06d sec\n", arg, dt.tv_sec, dt.tv_usec);
 }
 
 pthread_t limiteThreadCpu(pthread_t* thread){
@@ -53,29 +56,29 @@ int main() {
     perror("pthread_create() error");
     exit(1);
   }
-  param.sched_priority = 20;
+  /*param.sched_priority = 20;
   pthread_attr_setschedparam (&attr, &param);
-    if (pthread_create(&tTwo, &attr, handler, 2) != 0) {
+ //   if (pthread_create(&tTwo, &attr, handler, 2) != 0) {
     perror("pthread_create() error");
     exit(1);
   }
   param.sched_priority = 30;
   pthread_attr_setschedparam (&attr, &param);
-    if (pthread_create(&tThree, &attr, handler, 3)!= 0) {
+   // if (pthread_create(&tThree, &attr, handler, 3)!= 0) {
     perror("pthread_create() error");
     exit(1);
   }
   param.sched_priority = 40;
   pthread_attr_setschedparam (&attr, &param);
-    if (pthread_create(&tFour, &attr, handler, 4) != 0) {
+    //if (pthread_create(&tFour, &attr, handler, 4) != 0) {
     perror("pthread_create() error");
     exit(1);
   };
 
   pthread_join(tOne, NULL);
   pthread_join(tTwo, NULL);
-  pthread_join(tThree, NULL);
-  pthread_join(tFour, NULL);
+  pthread_join(tThree, NULL);*/
+  pthread_join(tOne, NULL);
 
   return 0;
 }
